4aee27bc22b5a6b5640d1e0c07229da9dd08c900
Patrick Smyth <patrick.smyth@chainguard.dev>
2024-04-16 16:04:02 -0400

    Linted code, checked imports

--- run.py
+++ run.py
@@ -1,76 +1,81 @@
-from sys import argv
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torch.backends.cudnn as cudnn
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
 import os
-from PIL import Image
+import time
 from tempfile import TemporaryDirectory
 from pathlib import Path
+from sys import argv
+import torch
+from torch import nn, optim
+from torch.optim import lr_scheduler
+from torch.backends import cudnn
+from torchvision import datasets, models, transforms
+from PIL import Image
 
 
 
 cudnn.benchmark = True
-plt.ion()   # interactive mode
+
 
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
+    "train": transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
 }
 
-data_dir = 'data/octopus-penguin-whale'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+data_dir = "data/octopus-penguin-whale"
+image_datasets = {
+    x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+    for x in ["train", "val"]
+}
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=4, shuffle=True, num_workers=4
+    )
+    for x in ["train", "val"]
+}
+dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+class_names = image_datasets["train"].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 ####
 
+
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
     # Create a temporary directory to save training checkpoints
     with TemporaryDirectory() as tempdir:
-        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+        best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
 
         torch.save(model.state_dict(), best_model_params_path)
         best_acc = 0.0
 
         for epoch in range(num_epochs):
-            print(f'Epoch {epoch}/{num_epochs - 1}')
-            print('-' * 10)
+            print(f"Epoch {epoch}/{num_epochs - 1}")
+            print("-" * 10)
 
             # Each epoch has a training and validation phase
-            for phase in ['train', 'val']:
-                if phase == 'train':
+            for phase in ["train", "val"]:
+                if phase == "train":
                     model.train()  # Set model to training mode
                 else:
-                    model.eval()   # Set model to evaluate mode
+                    model.eval()  # Set model to evaluate mode
 
                 running_loss = 0.0
                 running_corrects = 0
@@ -85,47 +90,48 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                     # forward
                     # track history if only in train
-                    with torch.set_grad_enabled(phase == 'train'):
+                    with torch.set_grad_enabled(phase == "train"):
                         outputs = model(inputs)
 
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
-                        if phase == 'train':
+                        if phase == "train":
                             loss.backward()
                             optimizer.step()
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
-                if phase == 'train':
+                if phase == "train":
                     scheduler.step()
 
                 epoch_loss = running_loss / dataset_sizes[phase]
                 epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+                print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
 
                 # deep copy the model
-                if phase == 'val' and epoch_acc > best_acc:
+                if phase == "val" and epoch_acc > best_acc:
                     best_acc = epoch_acc
                     torch.save(model.state_dict(), best_model_params_path)
 
             print()
 
         time_elapsed = time.time() - since
-        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f'Best val Acc: {best_acc:4f}')
+        print(
+            f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s"
+        )
+        print(f"Best val Acc: {best_acc:4f}")
 
         # load best model weights
         model.load_state_dict(torch.load(best_model_params_path))
     return model
 
 
-
 def run_training():
-    model_ft = models.resnet18(weights='IMAGENET1K_V1')
+    model_ft = models.resnet18(weights="IMAGENET1K_V1")
     num_ftrs = model_ft.fc.in_features
 
     # Here the size of each output sample is set to 2.
@@ -143,18 +149,19 @@ def run_training():
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
     # Train and evaluate:
-    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                           num_epochs=25)
+    model_ft = train_model(
+        model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25
+    )
 
     return model_ft
 
 
-def infer_class(model,img_path):
+def infer_class(model, img_path):
 
     model.eval()
 
     img = Image.open(img_path)
-    img = data_transforms['val'](img)
+    img = data_transforms["val"](img)
     img = img.unsqueeze(0)
     img = img.to(device)
 
@@ -164,26 +171,16 @@ def infer_class(model,img_path):
 
         return class_names[preds]
 
-        ax = plt.subplot(2,2,1)
-        ax.axis('off')
-        ax.set_title(f'Predicted: {class_names[preds[0]]}')
-        imshow(img.cpu().data[0])
 
-        model.train(mode=was_training)
-
-
-if __name__ == '__main__':
-    model_file = Path('octopus_whale_penguin_model.pt')
+if __name__ == "__main__":
+    model_file = Path("octopus_whale_penguin_model.pt")
     if model_file.exists():
         model = torch.load(model_file)
     else:
         model = run_training()
-        torch.save(model, 'octopus_whale_penguin_model.pt')
-
+        torch.save(model, "octopus_whale_penguin_model.pt")
 
     if len(argv) >= 2:
         input_file = Path(argv[1])
         if input_file.exists():
             print(infer_class(model, input_file))
-        
-
